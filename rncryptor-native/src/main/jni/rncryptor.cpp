4c451781fb43ea7c41e45e9f57f11c47199483df
mrgowtham3021 <124440407+mrgowtham3021@users.noreply.github.com>
2023-06-22 19:22:55 +0530

    Update rncryptor.cpp

--- rncryptor-native/src/main/jni/rncryptor.cpp
+++ rncryptor-native/src/main/jni/rncryptor.cpp
@@ -35,7 +35,7 @@ using CryptoPP::Base64Encoder;
 using CryptoPP::Base64Decoder;
 
 RNCryptor::RNCryptor() {
-	configureSettings(SCHEMA_2);
+	configureSettings(SCHEMA_3);
 }
 
 void RNCryptor::configureSettings(RNCryptorSchema schemaVersion)
@@ -59,6 +59,13 @@ void RNCryptor::configureSettings(RNCryptorSchema schemaVersion)
 			break;
 
 		case SCHEMA_2:
+			aesMode = MODE_CBC;
+			options = OPTIONS_1;
+			hmac_includesHeader = true;
+			hmac_includesPadding = false;
+			hmac_algorithm = HMAC_SHA256;
+			break;
+		case SCHEMA_3:
 			aesMode = MODE_CBC;
 			options = OPTIONS_1;
 			hmac_includesHeader = true;
